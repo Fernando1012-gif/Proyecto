cca5c8bf35f990e943964b2c723fbf855e5e96e0
Yzy-zu <knzz.uwu@gmail.com>
2026-03-16 19:08:21 -0600

    correccion errores en validacion fin de semana

--- backend/validacion/valMes.js
+++ backend/validacion/valMes.js
@@ -2,18 +2,29 @@ const dayjs = require('dayjs');
 
 const validaciones = {
     vDia: async (req, res, next) => {
-        const { pFecha} = req.body;
-        try{
-            const dia = dayjs(pFecha).day();
-            if(dia === 0 || dia === 6) {
+        const { fI } = req.body; 
+        if (!fI) {
+            return res.status(400).json({
+                ok: false,
+                msg: "se ocupa una fecha de inicio"
+            });}
+        try {
+            const dia = dayjs(fI).day();
+
+            if (dia === 0 || dia === 6) {
                 return res.status(400).json({
                     ok: false,
-                    msg: "Es fin de semana"
-                });}
-            req.fecha = {pFecha};
+                    msg: "no se permiten pases para fines de semana :v "
+                });
+            }
             next();
-        } catch(error){
-            res.status(500).json({ok: false, msg: "Error en el servidor"});
-            }}
+        
+        } catch (error) {
+            console.error();
+            res.status(500).json({
+                ok: false, 
+                msg: "error al validar la fecha en el servidor"
+            });} }
 }
+
 module.exports = validaciones;
